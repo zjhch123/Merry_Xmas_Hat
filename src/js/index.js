8c385e23eba690db865304579007f9d34b0e172f
zjhch123 <416836508@qq.com>
2019-12-24 23:20:27 +0800

    Bugfix: black border

--- src/js/index.js
+++ src/js/index.js
@@ -133,25 +133,8 @@ const directionManagement = (() => {
   }
 })()
 
-const base64Img2Blob = (code) => {
-  const parts = code.split(';base64,')
-  const contentType = parts[0].split(':')[1]
-  const raw = window.atob(parts[1])
-  const rawLength = raw.length
-
-  const uInt8Array = new Uint8Array(rawLength)
-
-  for (let i = 0; i < rawLength; i += 1) {
-    uInt8Array[i] = raw.charCodeAt(i)
-  }
-
-  return new Blob([uInt8Array], { type: contentType })
-}
-
 const downloadFile = (content) => {
-  const blob = base64Img2Blob(content)
-  const href = URL.createObjectURL(blob)
-  $modalImg.attr('src', href)
+  $modalImg.attr('src', content)
   $modal.show()
 }
 
